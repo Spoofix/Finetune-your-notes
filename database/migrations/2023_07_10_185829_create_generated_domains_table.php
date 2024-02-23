9ce303bfb775a7f216e8ab846fcf6edfe0b676e6
DavidWaichari <dwaichari@gmail.com>
2024-02-23 13:38:34 +0300

    upload multiple screenshots for the same domain for multiple scans

--- database/migrations/2023_07_10_185829_create_generated_domains_table.php
+++ database/migrations/2023_07_10_185829_create_generated_domains_table.php
@@ -12,10 +12,56 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('spoofed_domains', function (Blueprint $table) {
+           
             $table->id();
-            $table->integer('domain_id');
-            $table->string('spoofed_domain');
-            $table->integer("last_batch");
+            $table->unsignedBigInteger('domain_id')->nullable();
+            $table->string('spoofed_domain')->nullable();
+            $table->integer('last_batch')->nullable();
+            $table->string('rating')->nullable();
+            $table->string('screenshot')->nullable();
+            $table->string('country')->nullable();
+            $table->string('registrationDate')->nullable();
+            $table->string('registrar')->nullable();
+            $table->string('phashes')->nullable();
+            $table->string('htmls')->nullable();
+            $table->string('dir')->nullable();
+            $table->string('domain')->nullable();
+            $table->string('whois_server')->nullable();
+            $table->string('referral_url')->nullable();
+            $table->string('update_date')->nullable();
+            $table->string('expiration_date')->nullable();
+            $table->string('name_servers')->nullable();
+            $table->string('status')->nullable();
+            $table->string('emails')->nullable();
+            $table->string('dnssec')->nullable();
+            $table->string('name')->nullable();
+            $table->string('org')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('registrant_postal_code')->nullable();
+            $table->string('domainsimilarityrate')->nullable();
+            $table->string('csscolor')->nullable();
+            $table->string('domain_rating')->nullable();
+            $table->enum('spoof_status', ['SPOOFY', 'PHISHING', 'NOT_REPORTED', 'REPORTED'])->default('NOT_REPORTED');
+            $table->string('ip_address')->nullable();
+            $table->string('server_city')->nullable();
+            $table->string('region')->nullable();
+            $table->string('server_country')->nullable();
+            $table->double('latitude', 10, 6)->nullable();
+            $table->double('longitude', 10, 6)->nullable();
+            $table->string('organization')->nullable();
+            $table->string('isp')->nullable();
+            $table->string('server_os')->nullable();
+            $table->string('ssl_certificate_details')->nullable();
+            $table->string('redirect_urls')->nullable();
+            $table->string('http_status_code')->nullable();
+            $table->string('cookies')->nullable();
+            $table->longText('console_messages')->nullable();
+            $table->string('security_headers')->nullable();
+            $table->enum('spoof_status_new', ['scanned', 'inprogress', 'completed'])->default('scanned');
+            $table->enum('current_scan_status', ['scanned', 'not_scanned'])->default('not_scanned');
+            $table->enum('progress_status', ['new', 'monitoring', 'pending_authentication'])->default('new');
             $table->timestamps();
         });
     }
